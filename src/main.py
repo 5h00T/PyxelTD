d21e88dd46da284307e19b6bd267fd09ee4ce8fc
shoot <undefindshinjin@gmail.com>
2025-07-16 23:18:23 +0900

    main.pyを追加

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pyxel.cls(0)
+
+if __name__ == "__main__":
+    App()
